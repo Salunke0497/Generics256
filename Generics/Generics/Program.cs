0c14359c6c8380b322ac282afb9ab0caeb419c08
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-20 08:59:50 +0530

    [Amol] : Adding for Find Maximum String Values

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericMaximum genericClass = new GenericMaximum();
+
+            //for integer method 
+            int ans = genericClass.GenMaximum(5, 4, 6);
+            Console.WriteLine("the maximum num are : " + ans);
+            
+            //for float method
+            float value = genericClass.GenMaximum(7f,6f,9f);
+            Console.WriteLine("the maximum float value are : " + value);
+
+            //for string method
+            string answer = genericClass.GenMaximum("Apple","Banana","Mango");
+            Console.WriteLine("the maximum string value are : " + answer);
+            Console.ReadLine();
+        }
+    }
+}

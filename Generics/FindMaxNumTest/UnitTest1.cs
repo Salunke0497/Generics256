1ac2bae9298f3500d216d162eb6da504570918c6
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-21 16:41:40 +0530

    [Amol] : Adding For Find Maximum Generics for Int Num at 2nd position

--- Generics/FindMaxNumTest/UnitTest1.cs
+++ Generics/FindMaxNumTest/UnitTest1.cs
@@ -14,11 +14,21 @@ namespace FindMaxNumTest
         }
 
         //for Test case 1.1
+        //find maximum number at 1st position
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()
         {
             int answer = genericbaseInt.GenMaximum(5, 6, 7);
             Assert.AreEqual(7, answer);
+        }
+        
+        //for Test case 1.2
+        //find maximum number at secound position
+        [Test]
+        public void GivenMaxSecoundNum_WhenAnalysed_ShouldReturnSecoundMax()
+        {
+            int answer = genericbaseInt.GenMaximum(5, 9, 7);
+            Assert.AreEqual(9, answer);
         }
     }
 }

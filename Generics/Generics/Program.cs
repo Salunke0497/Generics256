2d98e8613eef024f84aaa5b0d341c61148b0d663
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-21 16:11:33 +0530

    [Amol] : Adding For Find Maximum Integer Num

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericMaximum genericClass = new GenericMaximum();
+
+            //for integer method 
+            //UC1
+            int ans = genericClass.GenMaximum(5, 4, 6);
+            Console.WriteLine("the maximum num are : " + ans);
+        }
+    }
+}
